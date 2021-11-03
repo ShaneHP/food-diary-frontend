ff961573741a8fab5e7ee1b53538b1497c369c57
Shane Hyland Pierce <shanehp98@gmail.com>
2021-11-03 20:50:31 +0000

    added register api call

--- providers/AuthProvider.js
+++ providers/AuthProvider.js
@@ -1,10 +1,39 @@
 import React, { createContext, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import axios from 'axios';
+import qs from 'qs';
+import { BASE_URL } from '@env';
 
 export const AuthContext = createContext({});
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
+
+    const register = async (email, password) => {
+        console.log('registering with email and password', email, password);
+
+        const options = {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/x-www-form-urlencoded',
+            },
+            data: qs.stringify({ email, password }),
+            url: `${BASE_URL}/signup`,
+        };
+
+        axios(options)
+            .then((res) => {
+                if (res.data.errors) {
+                    console.log(data.errors);
+                } else {
+                    console.log(res.data);
+                    AsyncStorage.setItem('jwt', res.data);
+                }
+            })
+            .catch((err) => {
+                console.log(err.response.data);
+            });
+    };
 
     const login = (email, password) => {
         const fakeUser = { email, password };
@@ -24,6 +53,7 @@ const AuthProvider = ({ children }) => {
                 login,
                 logout,
                 setUser,
+                register,
             }}
         >
             {children}
--- screens/Register.js
+++ screens/Register.js
@@ -1,22 +1,95 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { AuthContext } from '../providers/AuthProvider';
+import { globalStyles } from '../styles/global';
+import { Formik } from 'formik';
+import * as yup from 'yup';
 
-const Register = ({ navigation, route }) => {
+const registerSchema = yup.object({
+    email: yup.string().required('Email is required'),
+    password: yup.string().required('Password is required'),
+});
+
+const Register = ({ navigation }) => {
+    const { register } = useContext(AuthContext);
+
+    const onSubmit = (values, actions) => {
+        actions.resetForm();
+
+        const { email, password } = values;
+        register(email, password);
+    };
+
     return (
         <View
             style={{
-                flex: 1,
-                alignItems: 'center',
                 justifyContent: 'center',
+                flex: 1,
+                padding: 20,
             }}
         >
-            <Text>route name: {route.name} </Text>
-            <Button
-                title="go to login"
-                onPress={() => {
-                    navigation.navigate('Login');
+            <Text
+                style={{
+                    textAlign: 'center',
+                    fontSize: 30,
+                    marginBottom: 30,
                 }}
-            />
+            >
+                Register
+            </Text>
+            <Formik
+                validationSchema={registerSchema}
+                initialValues={{
+                    email: '',
+                    password: '',
+                }}
+                onSubmit={onSubmit}
+            >
+                {(formikProps) => (
+                    <View>
+                        <TextInput
+                            style={globalStyles.input}
+                            textContentType="emailAddress"
+                            placeholder="Email"
+                            onChangeText={formikProps.handleChange('email')}
+                            value={formikProps.values.email}
+                        />
+                        <Text style={globalStyles.errorText}>
+                            {formikProps.touched.email &&
+                                formikProps.errors.email}
+                        </Text>
+                        <TextInput
+                            textContentType="password"
+                            secureTextEntry={true}
+                            style={globalStyles.input}
+                            placeholder="Password"
+                            onChangeText={formikProps.handleChange('password')}
+                            value={formikProps.values.password}
+                        />
+                        <Text style={globalStyles.errorText}>
+                            {formikProps.touched.password &&
+                                formikProps.errors.password}
+                        </Text>
+
+                        <Button
+                            title="Register"
+                            onPress={formikProps.handleSubmit}
+                        />
+                    </View>
+                )}
+            </Formik>
+            <View
+                style={{
+                    paddingTop: 20,
+                }}
+            >
+                <Button
+                    title="go back to login page"
+                    onPress={() => {
+                        navigation.navigate('Login');
+                    }}
+                />
+            </View>
         </View>
     );
 };
